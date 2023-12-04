1a843e32c1c60710ad8f265883e91dd8fe800ea1
askvyas <vyasakondi009@gmail.com>
2023-12-03 20:53:39 -0500

    added .gitignore and code to generate testcases in pdf format

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import os
 import openai
 from dotenv import load_dotenv
+from fpdf import FPDF
+
 load_dotenv() 
 openai.api_key = os.getenv("OPENAI_API_KEY") 
 msgs = []
-# system_msg = input("What type of chatbot would you like to create?\n")
-# msgs.append ({"role": "system", "content": system_msg})
-# print("Say hello to your new chatbot! Type quit() when done.")
-# while True:
-#     msg = input("YOU: ")
-#     if "quit()" in msg:
-#         break
-#     msgs.append ({"role": "user", "content": msg})
-#     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=msgs)
-#     reply = response["choices"][0]["message"]["content"]
-#     msgs.append({"role": "assistant", "content": reply})
-#     print("\nAI: " + reply + "\n")
 
-
-
-
+# Main function that interacts with GPT-3.5-turbo
 def get_test_cases(app,format,info):
-    msg="Please give me test cases for "+app+" Application in the given format :" +format + ". Also consider this additional information"+info
+    msg="Please give me test cases for "+app+" Application in the given format :" +format + ". Also consider this additional information"+info+ " Aditionally I want each test case inside flower brackets, if test case has lost of steps put [] inside for each step"
     msgs.append ({"role": "user", "content": msg})
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=msgs)
     test_cases=response["choices"][0]["message"]["content"]
 
-    return test_cases
+    #generate pdf using fpdf
+
+    pdf_file=convert_to_pdf(test_cases)
+
+    return pdf_file
+
+
+# returning text on the webpage
+    # return test_cases
+
+
+def convert_to_pdf(text,filename="testcases.pdf"):
+    pdf=FPDF()
+    pdf.add_page()  
+    pdf.set_font("Arial", size = 12)  
+    pdf.multi_cell(0, 10, text)
+    pdf.output(filename)
+    return filename
+
+
